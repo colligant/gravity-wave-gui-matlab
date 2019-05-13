c314993c5d28e72009a82e5bc02c32e4c82b1738
Thomas Henry Colligan IV <thomas.colligan@umontana.edu>
2019-05-13 15:12:03 -0600

    Forgot to add files

---
+++ findMinimaClosestToIndex.m
@@ -0,0 +1,20 @@
+function [id1, id2] = findMinimaClosestToIndex(array, index)
+%   Gets the two closest minima on either side of index in array.
+%   Returns the indices into the array of the minima.
+[~, locs] = findpeaks(-array);
+locs = sort(locs);
+id2 = 0;
+id1 = 0;
+if size(locs, 1) == 1
+    sz = size(locs, 2);
+else
+    sz = size(locs, 1);
+end
+for i=2:sz
+    if (locs(i-1) < index) && (locs(i) > index)
+        id1 = locs(i-1);
+        id2 = locs(i);
+    end
+end
+end
+
